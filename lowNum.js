9b11c702e4904c696e9fb8ed2e2a822fe380ef1b
Barry Lam <blam452@gmail.com>
2018-10-18 22:34:13 -0400

    Add comment to lowNum

--- lowNum.js
+++ lowNum.js
@@ -1,22 +1,22 @@
 // let a = [1,4,1,2,3,6]
-let a = [-1, -2]
+let a = [-1, -2];
 // console.log(a)
-function lowInt(a){
-    let sortA = a.sort()
-    let min = 1
-    // console.log(sortA)
-    for(let i=0; i <= sortA.length; i++){
-        // console.log(sortA[i])
-        if (sortA.includes(min)){
-        
-            min += 1
-            // console.log('this number',min)
-        } else {
-            // console.log('this is the number we want',min)
-            return min
-        } 
-    } 
-    
+//  low number function.
+
+function lowInt(a) {
+  let sortA = a.sort();
+  let min = 1;
+  // console.log(sortA)
+  for (let i = 0; i <= sortA.length; i++) {
+    // console.log(sortA[i])
+    if (sortA.includes(min)) {
+      min += 1;
+      // console.log('this number',min)
+    } else {
+      // console.log('this is the number we want',min)
+      return min;
+    }
+  }
 }
 
-console.log( lowInt(a) )
+console.log(lowInt(a));
